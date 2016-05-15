b8a8d778483afe0cc9566b6b5aa06628e3fbf9e4
matcracker <matcracker98@gmail.com>
2016-05-15 18:06:56 +0200

    Added Rescuer for create and restore your server backup

---
+++ src/com/matcracker/PMManagerServers/Manager/Editing/Rescuer.java
@@ -0,0 +1,102 @@
+package com.matcracker.PMManagerServers.Manager.Editing;
+
+import java.io.File;
+
+import com.matcracker.PMManagerServers.API.StatusAPI;
+import com.matcracker.PMManagerServers.API.UtilityServersAPI;
+import com.matcracker.PMManagerServers.Languages.BaseLang;
+import com.matcracker.PMManagerServers.Utility.Utility;
+import com.matcracker.PMManagerServers.Utility.Zipper;
+
+import net.lingala.zip4j.exception.ZipException;
+
+public class Rescuer {
+   /* _____           _        _   __  __ _                   __  __                                   _____                              
+	*|  __ \         | |      | | |  \/  (_)                 |  \/  |                                 / ____|                             
+	*| |__) |__   ___| | _____| |_| \  / |_ _ __   ___ ______| \  / | __ _ _ __   __ _  __ _  ___ _ _| (___   ___ _ ____   _____ _ __ ___ 
+	*|  ___/ _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \______| |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__\___ \ / _ \ '__\ \ / / _ \ '__/ __|
+	*| |  | (_) | (__|   <  __/ |_| |  | | | | | |  __/      | |  | | (_| | | | | (_| | (_| |  __/ |  ____) |  __/ |   \ V /  __/ |  \__ \
+	*|_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|      |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_| |_____/ \___|_|    \_/ \___|_|  |___/
+	*                                                                                   __/ |                                             
+	*                                                                                  |___/                                              
+	*Copyright (C) 2015-2016 @author matcracker
+	*
+	*This program is free software: you can redistribute it and/or modify 
+	*it under the terms of the GNU Lesser General Public License as published by 
+	*the Free Software Foundation, either version 3 of the License, or 
+	*(at your option) any later version.
+	*/
+	
+	public static final String backupedServersPath = "Backups" + File.separator + "Servers";
+	public static final String extractedServersPath = "Backups" + File.separator + "Servers" + File.separator + "Extracted";
+	
+	public static void rescuerMenu(){
+		Utility.cleanScreen();
+		System.out.println(Utility.softwareName);
+		System.out.println(BaseLang.translate("pm.title.rescuer"));
+		System.out.println("1- " + BaseLang.translate("pm.rescuer.backup"));
+		System.out.println("2- " + BaseLang.translate("pm.rescuer.restore"));
+		System.out.println("3- " + BaseLang.translate("pm.standard.back"));
+		
+		int sel = Utility.readInt(BaseLang.translate("pm.chooise.option") + " ", null);
+		
+		if(sel == 1)
+			backup();
+		if(sel == 2)
+			restore();
+			
+		if(sel == 3)
+			return;
+	}
+	
+	private static void restore() {
+		Utility.cleanScreen();
+		System.out.println(Utility.softwareName);
+		System.out.println(BaseLang.translate("pm.title.restoreen.ini"));
+		Utility.showServers();
+		int server = Utility.readInt(BaseLang.translate("pm.chooise.server"), null);
+		
+		if(server >= UtilityServersAPI.getNumberServers())
+			backup();
+		else{
+			if(UtilityServersAPI.checkServersFile("Path", "path_", server)){
+				String pathContent = UtilityServersAPI.getPath(server);
+				if(pathContent != null){
+					Zipper.unzip(pathContent, backupedServersPath, null);
+					
+				}else
+					Utility.waitConfirm(BaseLang.translate("pm.errors.pathNull"));
+			}else
+				Utility.waitConfirm(BaseLang.translate("pm.errors.pathNotFound"));
+			
+		}
+	}
+
+	private static void backup(){
+		Utility.cleanScreen();
+		System.out.println(Utility.softwareName);
+		System.out.println(BaseLang.translate("pm.title.backup"));
+		Utility.showServers();
+		int server = Utility.readInt(BaseLang.translate("pm.chooise.server"), null);
+		
+		if(server >= UtilityServersAPI.getNumberServers())
+			backup();
+		else{
+			if(UtilityServersAPI.checkServersFile("Path", "path_", server)){
+				String pathContent = UtilityServersAPI.getPath(server);
+				if(pathContent != null){
+					try{
+						Zipper.zip(pathContent, backupedServersPath, null);
+						StatusAPI.setBackuped("Backuped", server);
+					}catch (ZipException e){
+						e.printStackTrace();
+					}
+					
+				}else
+					Utility.waitConfirm(BaseLang.translate("pm.errors.pathNull"));
+			}else
+				Utility.waitConfirm(BaseLang.translate("pm.errors.pathNotFound"));
+			
+		}
+	}
+}
---
+++ src/com/matcracker/PMManagerServers/Utility/Zipper.java
@@ -0,0 +1,69 @@
+package com.matcracker.PMManagerServers.Utility;
+
+import java.io.File;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+
+public class Zipper{
+   /* _____           _        _   __  __ _                   __  __                                   _____                              
+	*|  __ \         | |      | | |  \/  (_)                 |  \/  |                                 / ____|                             
+	*| |__) |__   ___| | _____| |_| \  / |_ _ __   ___ ______| \  / | __ _ _ __   __ _  __ _  ___ _ _| (___   ___ _ ____   _____ _ __ ___ 
+	*|  ___/ _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \______| |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__\___ \ / _ \ '__\ \ / / _ \ '__/ __|
+	*| |  | (_) | (__|   <  __/ |_| |  | | | | | |  __/      | |  | | (_| | | | | (_| | (_| |  __/ |  ____) |  __/ |   \ V /  __/ |  \__ \
+	*|_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|      |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_| |_____/ \___|_|    \_/ \___|_|  |___/
+	*                                                                                   __/ |                                             
+	*                                                                                  |___/                                              
+	*Copyright (C) 2015-2016 @author matcracker
+	*
+	*This program is free software: you can redistribute it and/or modify 
+	*it under the terms of the GNU Lesser General Public License as published by 
+	*the Free Software Foundation, either version 3 of the License, or 
+	*(at your option) any later version.
+	*/
+	
+	public static void zip(String targetFolderPath, String destinationFilePath, String password) throws ZipException{
+        ZipParameters parameters = new ZipParameters();
+        parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+        parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+        
+        if(password != null){
+        	if(password.length() > 0){
+                parameters.setEncryptFiles(true);
+                parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+                parameters.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+                parameters.setPassword(password);
+        	}
+        }
+
+        ZipFile zipFile = new ZipFile(destinationFilePath);
+
+        File targetFile = new File(targetFolderPath);
+        
+        if(targetFile.isFile()){
+            zipFile.addFile(targetFile, parameters);
+            System.out.println("File");
+        }else if(targetFile.isDirectory()){
+            zipFile.addFolder(targetFile, parameters);
+            System.out.println("DIR");
+            
+        }
+    }
+
+    public static void unzip(String targetZipFilePath, String destinationFolderPath, String password) {
+        try{
+            ZipFile zipFile = new ZipFile(targetZipFilePath);
+            
+            if (zipFile.isEncrypted()) {
+                zipFile.setPassword(password);
+            }
+            
+            zipFile.extractAll(destinationFolderPath);
+            
+        }catch (ZipException e){
+            e.printStackTrace();
+        }
+    }
+}
