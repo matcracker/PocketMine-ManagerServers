195af9a87ea9a8e42e00f2e59a710f13989a503a
matcracker <matcracker98@gmail.com>
2016-05-16 23:33:41 +0200

    Alpha 5!

--- src/com/matcracker/PMManagerServers/Main.java
+++ src/com/matcracker/PMManagerServers/Main.java
@@ -33,7 +33,7 @@ public class Main{
 	
 	public static void main(String[] args){
 		String menu = "", quit = "n";
-		String version = "1.0 Alpha 4";
+		String version = "1.0 Alpha 5";
 		
 		System.setOut(new PMPrintStream(System.out));
 		System.setErr(new PMPrintStream(System.out));
