5f4ccd41ce9761e46d353f6ec452ba10212f965e
matcracker <matcracker98@gmail.com>
2016-07-04 23:27:40 +0200

    Delete duplicated folders (part 1)

--- src/com/matcracker/PMManagerServers/commands/CommandStop.java
+++
@@ -1,9 +0,0 @@
-package com.matcracker.PMManagerServers.commands;
-
-import com.matcracker.PMManagerServers.utility.UtilityColor;
-
-public class CommandStop {
-	public static void command(String[] args){
-		System.out.println(UtilityColor.COLOR_PURPLE + "Not implemented yet!");
-	}
-}
